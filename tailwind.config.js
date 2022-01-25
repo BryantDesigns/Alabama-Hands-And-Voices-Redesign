57c4e8f9a4e58da7d377bdbf355d619fe6300d1d
Tyler Bryant <tbryantdesignsllc@gmail.com>
2022-01-24 22:29:12 -0700

    Add test comp

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,33 @@
 module.exports = {
-  content: [
-    './src/**/*.{html,js}',
-  ],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
       colors: {
-        transparent: 'transparent',
-        current: 'currentColor',
-        primary: '#141b4b ',
-        secondary: '#ff7f32 ',
-        'white': '#ffffff',
-        'silver': '#ecebff',
+        hvblue: {
+          DEFAULT: "#141B4B",
+          50: "#4859CE",
+          100: "#384BCA",
+          200: "#2E3EAC",
+          300: "#25328B",
+          400: "#1D276B",
+          500: "#141B4B",
+          600: "#080B1F",
+        },
+        hvorange: {
+          DEFAULT: "#FF7F32",
+          50: "#FFF2EA",
+          100: "#FFE5D5",
+          200: "#FFCBAC",
+          300: "#FFB284",
+          400: "#FF985B",
+          500: "#FF7F32",
+          600: "#F95D00",
+          700: "#C14800",
+          800: "#893300",
+          900: "#511E00",
+        },
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
-}
+  plugins: [],
+};
